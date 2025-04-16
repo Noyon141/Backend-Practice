cad7d3384a6af3dda8962a094bfc1481a186d772
Noyon141 <mdnoyon1590852@gmail.com>
2025-04-16 09:25:48 -0700

    Add error handling middleware and update environment variables for JWT support

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+import cookieParser from "cookie-parser";
 import express from "express";
 import connectToDatabase from "./database/mongodb.js";
 import { PORT } from "./env/env.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import userRouter from "./routes/user.routes.js";
@@ -9,9 +11,17 @@ import userRouter from "./routes/user.routes.js";
 
 const app = express();
 
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(cookieParser());
+
+
 app.use('/api/v1/auth',authRouter)
-app.use('/api/v1/subscription',subscriptionRouter)
-app.use('/api/v1/user',userRouter)
+app.use('/api/v1/subscriptions',subscriptionRouter)
+app.use('/api/v1/users',userRouter)
+
+app.use(errorMiddleware);
 
 app.get('/', (req, res) => {
     res.send('Welcome to Subscription Management System API');
